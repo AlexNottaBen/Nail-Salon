5506bb8a290d2f1cd091cb8c16b8b4450302fdb0
AlexNottaBen <alex.nottaben@gmail.com>
2023-12-02 18:19:40 +0200

    Added Signup and Login forms.

--- main.py
+++ main.py
@@ -17,6 +17,16 @@ def show_credits_page() -> str:
     return render_template("credits.html")
 
 
+@app.route("/signup")
+def show_signup_page() -> str:
+    return render_template("signup.html")
+
+
+@app.route("/login")
+def show_login_page() -> str:
+    return render_template("login.html")
+
+
 def main() -> None:
     app.run(debug=True, port=5000)
 
