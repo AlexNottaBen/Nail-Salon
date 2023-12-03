0914fd9b3e3b64dea8904e69d45d51bdddffd60c
AlexNottaBen <alex.nottaben@gmail.com>
2023-12-03 17:50:53 +0200

    Added .env with requirements.

--- app.py
+++ app.py
@@ -1,12 +1,17 @@
 #! /usr/bin/python3
 # -*- coding: utf-8 -*-
 
+from os import getenv
+
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from dotenv import load_dotenv
+
+load_dotenv()
 
 app = Flask(
     import_name=__name__,
     template_folder="templates",
     static_folder="static"
 )
-app.config['SECRET_KEY'] = 'your_secret_key_here'
+app.config['SECRET_KEY'] = getenv('SECRET_KET')
